fb2bce16610cc23b24f82d72d17fd1a30f5576bc
jin66 <dbf6609@naver.com>
2024-10-07 01:27:15 +0900

    feat : 도커컴포즈 및 init.sql 작성해서 배포 및 테스트환경 구축 (redis,mongoDB 까지 준비완료)

--- com.nameslowly.coinauctions.auth/src/main/java/com/nameslowly/coinauctions/auth/infrastructure/config/RedisConfig.java
+++ com.nameslowly.coinauctions.auth/src/main/java/com/nameslowly/coinauctions/auth/infrastructure/config/RedisConfig.java
@@ -4,15 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // Serializer 설정
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+
+        // Hash Serializer 설정
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+
+        return redisTemplate;
     }
 
 }
--- init.sql
+++ init.sql
@@ -17,17 +17,14 @@ FLUSH PRIVILEGES;
 CREATE TABLE IF NOT EXISTS p_user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,  -- 사용자 이름은 고유해야 함
-    password VARCHAR(100) NOT NULL,         -- 비밀번호
-    email VARCHAR(100) NOT NULL UNIQUE,     -- 이메일 필드 추가
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- 생성일
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- 수정일
-    );
+    password VARCHAR(100) NOT NULL         -- 비밀번호
+);
 
 -- 초기 데이터 삽입
-INSERT INTO p_user (username, password, email) VALUES
-    ('user1', 'password1', 'user1@example.com'),
-    ('user2', 'password2', 'user2@example.com'),
-    ('user3', 'password3', 'user3@example.com');
+INSERT INTO p_user (username, password) VALUES
+    ('user1', 'password1'),
+    ('user2', 'password2'),
+    ('user3', 'password3');
 
 CREATE TABLE IF NOT EXISTS p_user2 (
     id INT AUTO_INCREMENT PRIMARY KEY,
