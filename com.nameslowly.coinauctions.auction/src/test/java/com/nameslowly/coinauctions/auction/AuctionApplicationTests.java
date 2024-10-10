d08982e78424be90d9473629b4ff7e01aeed531e
jin66 <dbf6609@naver.com>
2024-10-10 11:43:40 +0900

    docker compose 초기 세팅(빌드완)

--- com.nameslowly.coinauctions.auction/src/test/java/com/nameslowly/coinauctions/auction/AuctionApplicationTests.java
+++ com.nameslowly.coinauctions.auction/src/test/java/com/nameslowly/coinauctions/auction/AuctionApplicationTests.java
@@ -2,7 +2,9 @@ package com.nameslowly.coinauctions.auction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AuctionApplicationTests {
 
