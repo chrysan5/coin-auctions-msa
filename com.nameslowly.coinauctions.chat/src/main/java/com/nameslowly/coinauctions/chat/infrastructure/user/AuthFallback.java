937fc54f6488e8d422a0df6867ad9f6447ce811e
chrysan5 <alswn5406@naver.com>
2024-10-13 22:12:50 +0900

    [feat] nickname 사용을 위한 유저정보 가져오기

---
+++ com.nameslowly.coinauctions.chat/src/main/java/com/nameslowly/coinauctions/chat/infrastructure/user/AuthFallback.java
@@ -0,0 +1,17 @@
+package com.nameslowly.coinauctions.chat.infrastructure.user;
+
+import com.nameslowly.coinauctions.common.exception.GlobalException;
+import com.nameslowly.coinauctions.common.response.ResultCase;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AuthFallback implements AuthFeignClient {
+
+    @Override
+    public UserInfoResponseDto getUser(String username) {
+        log.info("USER-SERVER 에러");
+        throw new GlobalException(ResultCase.NOT_FOUND_USER);
+    }
+}
