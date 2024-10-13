1443520e02b00ffa04bd90480fc1c94137686058
chrysan5 <alswn5406@naver.com>
2024-10-13 22:11:09 +0900

    [feat] 채팅방 과거 메시지 기록 조회 구현

--- com.nameslowly.coinauctions.chat/src/main/java/com/nameslowly/coinauctions/chat/domain/repository/ChatMessageRepository.java
+++ com.nameslowly.coinauctions.chat/src/main/java/com/nameslowly/coinauctions/chat/domain/repository/ChatMessageRepository.java
@@ -1,7 +1,11 @@
 package com.nameslowly.coinauctions.chat.domain.repository;
 
 import com.nameslowly.coinauctions.chat.domain.model.ChatMessage;
+import com.nameslowly.coinauctions.chat.domain.model.Chatroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findAllByChatroom(Chatroom chatroom);
 }
