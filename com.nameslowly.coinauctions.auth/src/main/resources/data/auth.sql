03e52543c52164827e22521c37f1b724f2fd1c25
Panjun-Kwon <pndlm1979@naver.com>
2024-10-17 11:36:38 +0900

    feat : user 더미 데이터 추가

---
+++ com.nameslowly.coinauctions.auth/src/main/resources/data/auth.sql
@@ -0,0 +1,7 @@
+INSERT INTO p_user (username, password, role)
+VALUES ('user1234', 'pwd1234@', 'MASTER'),
+       ('admin0000', 'admin0000@', 'MASTER'),
+       ('panjun1111', 'panjun1111@', 'MASTER'),
+       ('seongjin2222', 'seongjin2222@', 'MASTER'),
+       ('hyeokjin3333', 'hyeokjin3333@', 'MASTER'),
+       ('minju4444', 'minju4444@', 'MASTER');
