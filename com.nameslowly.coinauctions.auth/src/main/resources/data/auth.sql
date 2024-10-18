658fce9f0bc2bc3ac20de21bed2b738b07fdffb2
Panjun-Kwon <pndlm1979@naver.com>
2024-10-18 11:50:17 +0900

    auth 더미 데이터 추가

--- com.nameslowly.coinauctions.auth/src/main/resources/data/auth.sql
+++ com.nameslowly.coinauctions.auth/src/main/resources/data/auth.sql
@@ -1,7 +1,6 @@
 INSERT INTO p_user (username, password, role)
-VALUES ('user1234', 'pwd1234@', 'MASTER'),
-       ('admin0000', 'admin0000@', 'MASTER'),
-       ('panjun1111', 'panjun1111@', 'MASTER'),
-       ('seongjin2222', 'seongjin2222@', 'MASTER'),
-       ('hyeokjin3333', 'hyeokjin3333@', 'MASTER'),
-       ('minju4444', 'minju4444@', 'MASTER');
+VALUES ('admin0000', '$2a$10$EZyFIjdNsSr2ZXmchgcxIu0qCiw7B0iXESqVaOIT4uhXubBEvqsRm', 'MASTER'),
+       ('panjun1111', '$2a$10$EZyFIjdNsSr2ZXmchgcxIu0qCiw7B0iXESqVaOIT4uhXubBEvqsRm', 'MASTER'),
+       ('seongjin2222', '$2a$10$EZyFIjdNsSr2ZXmchgcxIu0qCiw7B0iXESqVaOIT4uhXubBEvqsRm', 'MASTER'),
+       ('hyeokjin3333', '$2a$10$EZyFIjdNsSr2ZXmchgcxIu0qCiw7B0iXESqVaOIT4uhXubBEvqsRm', 'MASTER'),
+       ('minju4444', '$2a$10$EZyFIjdNsSr2ZXmchgcxIu0qCiw7B0iXESqVaOIT4uhXubBEvqsRm', 'MASTER');
